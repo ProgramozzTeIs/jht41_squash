05bafef2e057563b6212fc17a26b01870881c722
Bakfic <nagybakfic@gmail.com>
2022-08-12 10:32:30 +0300

    add User class
    Filled user class with attributes, getters, setters and toString method

--- sb_squash_mvc/src/main/java/pti/sb_squash_mvc/model/User.java
+++ sb_squash_mvc/src/main/java/pti/sb_squash_mvc/model/User.java
@@ -1,5 +1,88 @@
 package pti.sb_squash_mvc.model;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="users")
+public class User {
+	
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="pwd")
+	private String pwd;
+	
+	@Column(name="type")
+	private String type;
+	
+	@Column(name="login")
+	private boolean login;
+	
+	@Column(name="firstlogin")
+	private boolean firstlogin;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public boolean isLogin() {
+		return login;
+	}
+
+	public void setLogin(boolean login) {
+		this.login = login;
+	}
+
+	public boolean isFirstlogin() {
+		return firstlogin;
+	}
+
+	public void setFirstlogin(boolean firstlogin) {
+		this.firstlogin = firstlogin;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", pwd=" + pwd + ", type=" + type + ", login=" + login
+				+ ", firstlogin=" + firstlogin + "]";
+	}
+	
 }
