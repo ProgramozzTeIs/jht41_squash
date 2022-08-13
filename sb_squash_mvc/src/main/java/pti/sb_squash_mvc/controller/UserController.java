67f546ddc7da2453c86be7fa7f282fd5f23be286
Tamas0007 <110929299+Tamas0007@users.noreply.github.com>
2022-08-13 21:36:16 +0200

    Update UserController
    2 new methods:
    searchLocation()
    changePassword()

--- sb_squash_mvc/src/main/java/pti/sb_squash_mvc/controller/UserController.java
+++ sb_squash_mvc/src/main/java/pti/sb_squash_mvc/controller/UserController.java
@@ -1,10 +1,13 @@
 package pti.sb_squash_mvc.controller;
 
+import java.io.IOException;
 import java.util.List;
 
+import org.jdom2.JDOMException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import pti.sb_squash_mvc.database.Database;
@@ -48,4 +51,82 @@ public class UserController {
 		return html;
 	}
 	
+	@GetMapping("/searchlocation")
+	public String searchLocation(Model model,
+			@RequestParam(name= "loggedInUserId") int userId,
+			@RequestParam(name= "locationId") int locationId
+			) throws JDOMException, IOException {
+		
+		String page = "";
+	
+		
+		Database db = new Database();
+		User user = db.getUserById(userId);
+		
+		if(user.isLogin() == false) {
+			
+			model.addAttribute("feedback", "you have to log in");
+			
+			page = "login.html";
+			
+		}else {
+			List<User> userList = db.getPlayers();
+			List<Game> gameList = db.getGamesByLocationId(locationId);
+			List<Location> locationList = db.getLocations();
+			
+			page = "user.html";
+			
+			model.addAttribute("user", user);
+			model.addAttribute("users", userList);
+			model.addAttribute("games", gameList);
+			model.addAttribute("locations",locationList);
+		}
+	
+		db.close();
+		
+		return page;
+	}
+	
+	@PostMapping("/change")
+	public String changePassword(Model model,
+			@RequestParam(name= "userId") int userId,
+			@RequestParam(name= "password") String pwd
+			) throws JDOMException, IOException {
+		
+		String page = "";
+		
+		Database db = new Database();
+		User user = db.getUserById(userId);
+		
+		
+		
+		if(user.isLogin() == false || user.getType().equals("user") == false) {
+			
+			
+			model.addAttribute("feedback", "you have to log in");
+			
+			page = "login.html";
+			
+		}else {
+			
+			user.setPwd(pwd);
+			db.updateUser(user);
+			List<User> userList = db.getPlayers();
+			List<Game> gameList = db.getGames();
+			List<Location> locationList = db.getLocations();
+			
+			page = "user.html";
+			
+			model.addAttribute("user", user);
+			model.addAttribute("users", userList);
+			model.addAttribute("games", gameList);
+			model.addAttribute("locations", locationList);	
+			
+		}
+		db.close();
+		
+
+		return page;
+	}
+
 }
