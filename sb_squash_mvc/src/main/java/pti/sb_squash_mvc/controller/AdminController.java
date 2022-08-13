84d4bc78db8097ca2d4af61a205590b711180a16
Takács Krisztián <tkrisz82@gmail.com>
2022-08-13 10:31:55 +0200

    full logout, half reguser

--- sb_squash_mvc/src/main/java/pti/sb_squash_mvc/controller/AdminController.java
+++ sb_squash_mvc/src/main/java/pti/sb_squash_mvc/controller/AdminController.java
@@ -1,8 +1,53 @@
 package pti.sb_squash_mvc.controller;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import pti.sb_squash_mvc.database.Database;
+import pti.sb_squash_mvc.model.User;
 
 @Controller
 public class AdminController {
+	
+	
+	@GetMapping("/admin/regplayer")
+	public String regPlayer(Model model, @RequestParam(name = "name") String name) {
+		
+		Database db = new Database();
+		
+		User user = null;
+		
+		user.setType("user");
+		
+		user.setName(name);
+		
+		user.setFirstlogin(true);
+		
+		user.setLogin(false);
+		
+		user.setId(0);
+		
+		Integer pwd = new Random().nextInt(1000);
+		
+		String password = pwd.toString();
+		
+		user.setPwd(password);
+		
+		db.regUser(user);
+		
+//		model.addAttribute("users", db.getPlayers());
+//		model.addAttribute("locations", db.getLocations());
+//		model.addAttribute("users", db.getGames());
+		
+		
+		db.close();
+		
+		
+		return "admin.html";
+	}
 
 }
--- sb_squash_mvc/src/main/java/pti/sb_squash_mvc/controller/LoginController.java
+++ sb_squash_mvc/src/main/java/pti/sb_squash_mvc/controller/LoginController.java
@@ -1,9 +1,34 @@
 package pti.sb_squash_mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import pti.sb_squash_mvc.database.Database;
+import pti.sb_squash_mvc.model.User;
 
 
 @Controller
 public class LoginController {
+	
+	@GetMapping("/logout")
+	public String logout(Model model, @RequestParam(name = "userid") int userId) {
+		
+		Database db = new Database();
+		
+		User user = db.getUserById(userId);
+		
+		user.setLogin(false);
+		
+		db.updateUser(user);
+		
+		db.close();
+		
+		model.addAttribute("feedback", "Succesful log out!");
+		
+		return "login.html";
+	}
+	
 
 }
